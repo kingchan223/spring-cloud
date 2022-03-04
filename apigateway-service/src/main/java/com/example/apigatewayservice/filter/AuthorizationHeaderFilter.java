165464785dda108f5613e1c6b8e71734de8282df
kingchan223 <kokiyo97@naver.com>
2022-03-04 21:04:09 +0900

    apigateway-service에 jwt 확인 추가

---
+++ apigateway-service/src/main/java/com/example/apigatewayservice/filter/AuthorizationHeaderFilter.java
@@ -0,0 +1,69 @@
+package com.example.apigatewayservice.filter;
+
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+    private final Environment env;
+
+    public AuthorizationHeaderFilter(Environment env) {
+        super(Config.class);
+        this.env = env;
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) ->{
+            ServerHttpRequest request = exchange.getRequest();
+
+            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)){
+                return onError(exchange, "no authorizationHeader", HttpStatus.UNAUTHORIZED);
+            }
+            String authorizationHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String jwt = authorizationHeader.replace("Bearer", "");
+            if(!isJwtValid(jwt)) return onError(exchange, "no authorizationHeader", HttpStatus.UNAUTHORIZED);
+
+            return chain.filter(exchange);
+        };
+    }
+
+
+    private boolean isJwtValid(String jwt) {
+        boolean retVal = true;
+        String subject = null;
+        try{
+            subject = Jwts.parser().setSigningKey(env.getProperty("token.secret"))
+                    .parseClaimsJws(jwt).getBody()
+                    .getSubject();
+        }catch(Exception ex){
+            retVal = false;
+        }
+        if(subject == null || subject.isEmpty()) retVal = false;
+        return retVal;
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus status) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(status);
+        log.error(err);
+        return response.setComplete();
+    }
+
+
+    public static class Config{
+
+    }
+}
