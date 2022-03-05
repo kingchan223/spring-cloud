8abc45ae5f9d904a5f8ab23167cb0ec24cce9638
kingchan223 <kokiyo97@naver.com>
2022-03-05 23:27:12 +0900

    spring cloud config

--- user-microservice/src/main/java/com/example/usermicroservice/security/WebSecurity.java
+++ user-microservice/src/main/java/com/example/usermicroservice/security/WebSecurity.java
@@ -24,6 +24,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception{
         http.csrf().disable();
 //        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/actuator/**").permitAll();
         http.authorizeRequests().antMatchers("/**")
                 .hasIpAddress("192.168.219.103")
                 .and()
